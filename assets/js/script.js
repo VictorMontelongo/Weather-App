e896ae85773ecc3cc3036fa4eada06628d652736
Victor Montelongo <victoramontelongo@gmail.com>
2023-10-22 21:12:29 -0500

    Added most of the generic style and code. need information to display. doublecheck api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
-var urlWeatherEndpoint = "https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=d7bcd9a8196a376ec618bad9fd98ff5d"
+const citySearch = document.querySelector('.search-city')
+const searchButton = document.querySelector('.search-btn')
+const cityName = citySearch
+const urlWeatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=Dallas&
+appid=d4f3cbc3f357360fd4b9df2654abf4ae`
+
 function currentWeather() {
-  fetch(urlWeatherEndpoint).then(function (res) {
-    if (res.ok) {
-      return res.json()
+  fetch(urlWeatherEndpoint).then(function (data) {
+    if (data.ok) {
+      return data.json()
     }
-    // console.log(res)
+    // console.log(data)
   }).then(function (weatherInfo) {
     forecastWeather(weatherInfo.coord.lat, weatherInfo.coord.lon);
     console.log(weatherInfo)
   })
 }
+// forecasting weather 
 function forecastWeather(lat, lon) {
-  var urlWeatherForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=d7bcd9a8196a376ec618bad9fd98ff5d`
+  var urlWeatherForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=d4f3cbc3f357360fd4b9df2654abf4ae`
   fetch(urlWeatherForecast).then(function (res) {
     if (res.ok) {
       return res.json()
@@ -20,5 +26,28 @@ function forecastWeather(lat, lon) {
     console.log(weatherInfo)
   })
 }
-// forecastWeather();
-currentWeather();
+
+// should be for the main card display
+const createWeatherCard = (cityName, weatherItem, index) => {
+  if (index === 0) {
+    return `<div class="details">
+      <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+      <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+      <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+      <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+      </div>
+        <div class="icon">
+        <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+        <h6>${weatherItem.weather[0].description}</h6>
+        </div>`;
+  } else { // HTML for the other five day forecast card
+    return `<li class="card">
+    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3> 
+    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+    </li>`;
+  }
+}
+searchButton.addEventListener('click', currentWeather);
