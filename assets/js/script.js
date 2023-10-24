d64079d9751a772f64a8b2fd620a5d4a5046a6fb
Victor Montelongo <victoramontelongo@gmail.com>
2023-10-23 21:34:51 -0500

    Updated css, html, API still not working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 const citySearch = document.querySelector('.search-city')
 const searchButton = document.querySelector('.search-btn')
-const cityName = citySearch
-const urlWeatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=Dallas&
-appid=d4f3cbc3f357360fd4b9df2654abf4ae`
+// const cityName = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=a1de2c11560a7edb71cd7169547ca85a`
 
-function currentWeather() {
+function currentWeather(cityName) {
+  const urlWeatherEndpoint = `https://api.openweathermap.org/data/3.0/weather?q=${cityName}&
+  appid=e99afe44363c30c28c06ae20b2b37ae9`
   fetch(urlWeatherEndpoint).then(function (data) {
     if (data.ok) {
       return data.json()
@@ -17,7 +17,7 @@ function currentWeather() {
 }
 // forecasting weather 
 function forecastWeather(lat, lon) {
-  var urlWeatherForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=d4f3cbc3f357360fd4b9df2654abf4ae`
+  var urlWeatherForecast = `https://api.openweathermap.org/data/3.0/forecast?lat=${lat}&lon=${lon}&appid=e99afe44363c30c28c06ae20b2b37ae9`
   fetch(urlWeatherForecast).then(function (res) {
     if (res.ok) {
       return res.json()
@@ -28,25 +28,25 @@ function forecastWeather(lat, lon) {
 }
 
 // should be for the main card display
-const createWeatherCard = (cityName, weatherItem, index) => {
+const createWeatherCard = (weatherInfo, index) => {
   if (index === 0) {
     return `<div class="details">
-      <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-      <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
-      <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
-      <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+      <h2>${cityName} (${weatherInfo.dt_txt.split(" ")[0]})</h2>
+      <h6>Temperature: ${(weatherInfo.main.temp - 273.15).toFixed(2)}°C</h6>
+      <h6>Wind: ${weatherInfo.wind.speed} M/S</h6>
+      <h6>Humidity: ${weatherInfo.main.humidity}%</h6>
       </div>
         <div class="icon">
-        <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-        <h6>${weatherItem.weather[0].description}</h6>
+        <img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@4x.png" alt="weather-icon">
+        <h6>${weatherInfo.weather[0].description}</h6>
         </div>`;
   } else { // HTML for the other five day forecast card
     return `<li class="card">
-    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3> 
-    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
-    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
-    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+    <h3>(${weatherInfo.dt_txt.split(" ")[0]})</h3> 
+    <img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@4x.png" alt="weather-icon">
+    <h6>Temp: ${(weatherInfo.main.temp - 273.15).toFixed(2)}°C</h6>
+    <h6>Wind: ${weatherInfo.wind.speed} M/S</h6>
+    <h6>Humidity: ${weatherInfo.main.humidity}%</h6>
     </li>`;
   }
 }
